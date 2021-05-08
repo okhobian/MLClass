3912b360ddd1dc6b9bd3b534ddae14383c4f8420
okhobian <[okhobian@gmail.com]>
2021-05-07 20:35:11 -0500

    update

--- 6_Neural_Networks/calculator.py
+++ 6_Neural_Networks/calculator.py
@@ -1,19 +1,19 @@
 import numpy as np
 
-A = np.array([[1,2,3],[3,2,1]])
-B = np.array([[0.5,0.1,0.3],[-1,-20,1.5]])
+A = np.array([[1,1/2,1/3],[1/3,1/2,1]])
+B = np.array([[0.5,1,6],[3,-4,2]])
 
 hadamard = np.multiply(A,B)
-# print(hadamard)
+print(hadamard)
 
 AB_T = np.matmul(A, B.T)
-# print(AB_T)
+print(AB_T)
 
 BA_T = np.matmul(B, A.T)
-# print(BA_T)
+print(BA_T)
 
-x = np.array([1,0,1,0]).T
-w = np.array([5,4,6,1]).T
-w_t_x = np.matmul(w.T, x.T)
+# x = np.array([1,0,1,0]).T
+# w = np.array([5,4,6,1]).T
+# w_t_x = np.matmul(w.T, x.T)
 # print(w_t_x)
-print(11**2)
+# print(11**2)
